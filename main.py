6e214fc4ddf9d3d65fd0deab34b09409affa4d44
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-13 18:41:05 +0200

    Added code for model tools, agent

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 
+load_dotenv()
 
+def main():
+    model = ChatOpenAI(temperature=0)
+    
+    tools = []
+
+    agent_executor = create_react_agent(model, tools)
 
 
 
