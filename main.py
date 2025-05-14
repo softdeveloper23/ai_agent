e1bb1becd2c5b76eb4a38a0ec48b01525bfb8fe6
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-14 16:40:57 +0200

    Updated code

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ load_dotenv()
 @tool
 def calculator(a: float, b: float) -> str:
     """Useful for performing basic arithmetic calculations with numbers"""
+    print(f"Calculating {a} + {b}...")
     return f"The sum of {a} and {b} is {a + b}"
 
 def main():
     model = ChatOpenAI(temperature=0)
     
-    tools = []
+    tools = [calculator]
 
     agent_executor = create_react_agent(model, tools)
 
