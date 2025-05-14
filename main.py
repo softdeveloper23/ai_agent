82989b8ffce904af66f51b2b3b5e52c5173b8a35
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-14 16:42:59 +0200

    Added say_hello function

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ def calculator(a: float, b: float) -> str:
     print(f"Calculating {a} + {b}...")
     return f"The sum of {a} and {b} is {a + b}"
 
+@tool
+def say_hello(name : str) -> str:
+    """Useful for greeting a user"""
+    print(f"Calculating...")
+    return f"Hello {name}! I hope you are doing well."
+
 def main():
     model = ChatOpenAI(temperature=0)
     
-    tools = [calculator]
+    tools = [calculator, say_hello]
 
     agent_executor = create_react_agent(model, tools)
 
